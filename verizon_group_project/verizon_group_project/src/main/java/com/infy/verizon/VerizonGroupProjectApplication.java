90ee502864350eb404de7eb1fb0e297440a19778
Christopher Beall <christopher.beall@ad.infosys.com>
2020-04-23 10:58:40 -0500

    Adding spring boot files to github

---
+++ verizon_group_project/verizon_group_project/src/main/java/com/infy/verizon/VerizonGroupProjectApplication.java
@@ -0,0 +1,13 @@
+package com.infy.verizon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerizonGroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerizonGroupProjectApplication.class, args);
+	}
+
+}
---
+++ verizon_group_project/verizon_group_project/src/test/java/com/infy/verizon/VerizonGroupProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infy.verizon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerizonGroupProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
