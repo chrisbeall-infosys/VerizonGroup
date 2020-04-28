6b0a078ae88d809c4ff674f5c01fdbbf0a571c09
Thu Vuong <thu.vuong@ad.infosys.com>
2020-04-28 10:54:37 -0500

    Initial running Front-end app

--- VerizonFlightBookingApp/src/app/app.module.ts
+++ VerizonFlightBookingApp/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminHomeComponent } from './admin/admin-home/admin-home/admin-home.component';
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
 import { AdminLandingPageComponent } from './admin/admin-landing-page/admin-landing-page.component';
 import { LoginComponent } from './admin/admin-landing-page/login/login.component';
 import { RegistrationComponent } from './admin/admin-landing-page/registration/registration.component';
