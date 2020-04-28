cf4309b62a080eb6a8831ee887635eab9b6de873
Thu Vuong <thu.vuong@ad.infosys.com>
2020-04-27 22:46:14 -0500

    Added admin-traveler components with models

--- VerizonFlightBookingApp/src/app/app.module.ts
+++ VerizonFlightBookingApp/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminHomeComponent } from './admin/admin-home/admin-home/admin-home.component';
+import { AdminLandingPageComponent } from './admin/admin-landing-page/admin-landing-page.component';
+import { LoginComponent } from './admin/admin-landing-page/login/login.component';
+import { RegistrationComponent } from './admin/admin-landing-page/registration/registration.component';
+import { TravelerHomeComponent } from './traveler/traveler-home/traveler-home.component';
+import { TravelerLandingPageComponent } from './traveler/traveler-landing-page/traveler-landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminHomeComponent,
+    AdminLandingPageComponent,
+    LoginComponent,
+    RegistrationComponent,
+    TravelerHomeComponent,
+    TravelerLandingPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ VerizonFlightBookingApp/src/app/shared/models/admin.ts
@@ -0,0 +1,12 @@
+export class Admin {
+    /*
+    private String loginId;
+	private String email;
+	private String name;
+	private String password;
+    */
+    loginId: string;
+    email: string;
+    name: string;
+    password: string;
+}
