cc20a4a04cbb89062dd7c9756d6f68857009f6df
Thu Vuong <thu.vuong@ad.infosys.com>
2020-04-28 22:15:12 -0500

    Added Admin/Traveler's login,register, and home pages

--- VerizonFlightBookingApp/src/app/app.module.ts
+++ VerizonFlightBookingApp/src/app/app.module.ts
@@ -3,26 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
-import { AdminLandingPageComponent } from './admin/admin-landing-page/admin-landing-page.component';
-import { LoginComponent } from './admin/admin-landing-page/login/login.component';
-import { RegistrationComponent } from './admin/admin-landing-page/registration/registration.component';
-import { TravelerHomeComponent } from './traveler/traveler-home/traveler-home.component';
-import { TravelerLandingPageComponent } from './traveler/traveler-landing-page/traveler-landing-page.component';
+import { AuthorizationErrorComponent } from './shared/authorization-error/authorization-error.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AdminModule } from './admin/admin.module';
+import { TravelerModule } from './traveler/traveler.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminHomeComponent,
-    AdminLandingPageComponent,
-    LoginComponent,
-    RegistrationComponent,
-    TravelerHomeComponent,
-    TravelerLandingPageComponent
+    AuthorizationErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    AdminModule,
+    TravelerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ VerizonFlightBookingApp/src/app/traveler/traveler-landing-page/login/login.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from "@angular/common/http";
+import { Traveler } from "../../../shared/models/traveler";
+import { environment } from "../../../../environments/environment";
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  private headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(private http: HttpClient) { }
+  login(traveler: Traveler): Observable<Traveler> {
+    const url = environment.travelerAPIUrl + '/travelerLogin';
+
+    return this.http.post<Traveler>(url,traveler,{headers:this.headers})
+    .pipe(catchError(this.handleError));
+
+}
+private handleError(err: HttpErrorResponse) {
+    console.log(err)
+    let errMsg:string='';
+    if (err.error instanceof Error) {   
+        errMsg=err.error.message;
+        console.log(errMsg)
+    }
+     else if(typeof err.error === 'string'){
+        errMsg=JSON.parse(err.error).message
+    }
+    else {
+       if(err.status==0){ 
+           errMsg="A connection to back end can not be established.";
+       }else{
+           errMsg=err.error.message;
+       }
+     }
+        return throwError(errMsg);
+}
+}
