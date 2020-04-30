387fcce34e45305c0d1bda79ad3bbfe57bf51ab5
Christopher Beall <christopher.beall@ad.infosys.com>
2020-04-30 10:45:56 -0500

    committing changes, fleshing out component

--- VerizonFlightBookingApp/src/app/booking/booking.component.ts
+++ VerizonFlightBookingApp/src/app/booking/booking.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '../../../node_modules/@angular/forms';
 import { BookingService } from './booking-service.service';
 import { Flight } from '../shared/models/Flight';
+import { Airport } from '../shared/models/airport';
 
 
 @Component({
@@ -11,13 +12,22 @@ import { Flight } from '../shared/models/Flight';
 })
 export class BookingComponent implements OnInit {
 
-  flightList: Flight[] = [];
-  airportList: Airport[] = [];
-  airportToList: Airport[] = [];
+  flightList: Flight;
+  airportList: Airport[];
+  airportMap: object;
+  airportToList: Airport[]; 
+
+  fromSelected(value: any){
+    
+  }
 
   constructor(private fb :FormBuilder, private bookingService: BookingService) { }
 
   ngOnInit() {
+    this.airportList = getAirportList();
+    for (let airport in this.airportList){
+      this.airportMap[airport] = getAirportToList();
+    }
   }
 
 }
