e696a392245e0d98e69b6355520bd810c4a1d197
Christopher Beall <christopher.beall@ad.infosys.com>
2020-05-01 15:19:41 -0500

    get flights service, setting up submission of a new booking (still needs logic work)

--- VerizonFlightBookingApp/src/app/traveler/booking/booking.component.ts
+++ VerizonFlightBookingApp/src/app/traveler/booking/booking.component.ts
@@ -5,6 +5,7 @@ import { BookingService } from './booking-service.service';
 import { BookingGetFlightsService } from './booking-get-flights.service';
 import { Flight } from '../../shared/models/Flight';
 import { Airport } from '../../shared/models/airport';
+import { Booking } from '../../shared/models/booking';
 
 
 @Component({
@@ -16,20 +17,43 @@ export class BookingComponent implements OnInit {
   newBookingForm: FormGroup;
   flightList: Flight[];
   airportList: Airport[];
+  
   airportMap: object;
   airportToList: Airport[];
+  selectedFlight: Flight;
+  successMessage:string;
+  errorMessage:string;
 
   fromSelected(value: any) {
     this.airportToList = this.airportMap[value];
   }
+
+  bookingSubmit(){
+    let booking:Booking = new Booking();
+    booking.dateOfTravel = this.newBookingForm.get('dateOfTravel').value;
+    booking.flight = this.selectedFlight;
+    booking.numberOfTravelers = this.newBookingForm.get('numberOfTravelers').value;
+    booking.traveler = JSON.parse(sessionStorage.getItem("traveler"));
+    this.bookingService.addNewBooking(booking).subscribe(
+      (success) => {
+        this.successMessage = "Deal Added!";
+      },
+      (error) => {
+        this.errorMessage = error.error.message;
+      }
+    )};
 
 
   constructor(private fb: FormBuilder, private bookingService: BookingService, private flightService: BookingGetFlightsService) { }
 
   ngOnInit() {
     this.newBookingForm = this.fb.group({
-      filler: ['', [Validators.required ]],
-      
+      flight: ['', [Validators.required]],
+      traveler: ['', [Validators.required]],
+      fromAirport: ['', [Validators.required]],
+      toAirport :['', [Validators.required]],
+      numberOfTravelers: ['', Validators.required],
+      dateOfTravel: ['', [Validators.required]]      
     });
 
     let flightList = this.flightService.getFlights();
