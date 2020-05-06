7230a6086c46f5d90d230b3ae5c9403c55ccd2a0
Jonathan Sisneros <jonathan.sisneros@ad.infosys.com>
2020-05-06 08:14:10 -0500

    updated flight front end

--- VerizonFlightBookingApp/src/app/admin/admin-home/add-flight/add-flight.component.ts
+++ VerizonFlightBookingApp/src/app/admin/admin-home/add-flight/add-flight.component.ts
@@ -28,7 +28,7 @@ export class AddFlightComponent implements OnInit {
   createForm(){
     this.addFlightForm = this.fb.group({
       //description: new FormControl("", [Validators.required, Validators.minLength(10)]),
-      flightId: ["", [Validators.required, Validators.min(1000)]],
+      flightId: ["", [Validators.required, Validators.min(0)]],
       fare: ["", [Validators.required, Validators.min(1)]],
       taxes: ["", [Validators.required, Validators.min(1)]],
       toAir: ["", Validators.required],
